0487ad264219cb2d7c790bc5c4bb0a0cb54908bd
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-12 21:10:17 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const list = document.querySelector(".gallery");
+const markup = galleryItems
+  .map((galleryItems) => `<a class="gallery__link" href="large-image.jpg"> <img class="gallery__image"
+  src=${galleryItems.preview}
+  description="${galleryItems.description}"> </a>`)
+  .join("");
+  list.insertAdjacentHTML('afterBegin', markup);
 console.log(galleryItems);
