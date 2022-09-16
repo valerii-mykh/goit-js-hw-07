58bd6b7605fcaa3fcfd8cfef15b7e95dd161d9f9
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-16 22:56:07 +0300

    Перероблення коду та внесення правок

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-const list = document.querySelector(".gallery");
-const markup = galleryItems
-  .map((galleryItems) => `<a class="gallery__link" href="large-image.jpg"> <img class="gallery__image"
-  src=${galleryItems.preview}
-  description="${galleryItems.description}"> </a>`)
-  .join("");
-  list.insertAdjacentHTML('afterBegin', markup);
 console.log(galleryItems);
+
+const gall = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items.map((item) => `<div class="gallery__item"> 
+  <a class="gallery__link" href="${item.original}"> 
+  <img class="gallery__image" 
+  src="${item.preview}" 
+  data-source="${item.original}" 
+  alt="${item.description}" /> 
+  </a> 
+  </div>`).join('');
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+gall.innerHTML = addGalleryMarkup;
+gall.addEventListener("click", onImageClick);
+function onImageClick(evt) {
+  // заборона відкривання картинки, браузером
+  blockStandartAction(evt);
+
+  if (evt.target.nodeName !== 'IMG'){
+    return;
+  }
+
+  const intance = basicLightbox.create(`
+  <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+  intance.show();
+
+  gall.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+intance.close();      
+    }
+  });
+}
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
