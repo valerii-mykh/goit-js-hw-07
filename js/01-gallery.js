a90498704e189bdf84994d0231968a44d8e05871
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-09-16 19:38:54 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const list = document.querySelector(".gallery");
 const markup = galleryItems
   .map((galleryItems) => `<a class="gallery__link" href="large-image.jpg"> <img class="gallery__image"
